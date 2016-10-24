8c7b4be92d0e950f6cf250485ba0b1d4fa43a7ba
Aryana Pezé <aryana.ravari@gmail.com>
2016-10-24 17:23:46 +0200

    cupcake website

--- app.rb
+++ app.rb
@@ -2,11 +2,31 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+
+
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
+# @repository = Repository.new
+# @controller = Controller.new(@repository)
 
 get '/' do
-  'Hello world!'
+  erb :about
+end
+
+get '/cupcakes' do
+  erb :cupakes
+end
+
+get '/' do
+  @controller.list
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
+end
+
+get '/create' do
+  @controller.create
 end
